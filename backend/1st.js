f378237d0fefa004dc4e6f766e628440ffd31783
arsenymolochay <apokapo217@gmail.com>
2023-10-30 14:58:22 +0200

    New file

---
+++ backend/1st.js
@@ -0,0 +1,89 @@
+//a=6
+
+// let a = 6
+// const b=6
+//
+// a = 7
+// b = 7
+
+// let str="asd"
+// let str1='asd'
+// let str2=`asd`
+//
+// let a = 5
+// let str4 = `a = ${a}`
+//
+// let number = 5
+// let number1 = 5.6
+//
+// let boolen = true
+// let boolen1 = false
+//
+// let array = [
+//     1,
+//     5,
+//     'ads',
+//     [
+//         6,
+//         7
+//     ]
+// ]
+//
+// let dict = {
+//     key:'value'
+// }
+// let dictValue = dict['key']
+// let dictValue1 = dict.key
+//
+// let dictValue2 = dict.number
+//
+// if (a==b){
+//
+// }
+// console.log(dictValue2);
+
+// while (true){
+//     break
+// }
+//
+// for (let i=0; i<7; i++){
+//     console.log(i);
+// }
+
+// for (const number of array) {
+//     console.log(number);
+// }
+
+// for (const arrayKey of array) {
+//     console.log(`${arrayKey}: ${<object[arrayKey]}`)
+// }
+
+// let a = 5
+// switch (a) {
+//     case 5:
+//         console.log(5);
+//         break;
+//     case 4:
+//         console.log(4);
+//         break;
+// }
+//
+// let a = 5
+// let c ='5'
+//
+// if(a===5 && c ===5){
+//     console.log('True')
+// }
+// else{
+//     console.log('False')
+// }
+
+/**
+ * Тестовая функция, которая что-то делает
+ * @param aaaaaaaaaaaa
+ * @param bbbbbbbbbbbbbbb
+ */
+function funkName(a,b){
+    console.log(1)
+}
+funkName()
